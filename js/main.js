2f0a822406c24d4d1099b97a123304f324ab0628
benditaguerrero <benditaguerrero@gmail.com>
2025-12-07 14:28:39 -0300

    inicio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+    console.log("Hola");
+
+
+    btnTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+
+
